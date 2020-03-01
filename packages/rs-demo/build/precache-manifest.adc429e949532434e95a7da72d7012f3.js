6f6f93365db3eae08b42506e09c898c3bf19abee
Ronny Siikaluoma <ronny.siikaluoma@husbanken.no>
2020-03-01 15:33:10 +0100

    add build folder to git

---
+++ packages/rs-demo/build/precache-manifest.adc429e949532434e95a7da72d7012f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b57dc207e6d2058b896c6aa01f7ddf37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f00f572ec192ed5eb2e3",
+    "url": "/static/css/main.18a35399.chunk.css"
+  },
+  {
+    "revision": "08d5f1eeae611ab5d2b6",
+    "url": "/static/js/2.89718ec8.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/static/js/2.89718ec8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f00f572ec192ed5eb2e3",
+    "url": "/static/js/main.9526b4b2.chunk.js"
+  },
+  {
+    "revision": "50bcc0459728992e0610",
+    "url": "/static/js/runtime-main.a585cdf2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
